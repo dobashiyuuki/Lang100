b3056343068b0c091e65de524f5ab17590c8f612
nabehiro <v_hirow@sigmact.com>
2015-03-18 08:06:25 +0900

    ch2-12

---
+++ Lang100/UnitTest2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Lang100
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        public static string _hightempText;
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            _hightempText = File.ReadAllText("hightemp.txt");
+        }
+
+        [TestMethod]
+        public void Test10()
+        {
+            // wc hightemp.txt
+            var result = _hightempText.Split('\n').Length;
+        }
+
+        [TestMethod]
+        public void Test11()
+        {
+            // cat hightemp.txt | sed -e 's/\t/ /g'
+            // cat hightemp.txt | tr '\t' ' '
+            var result = _hightempText.Replace('\t', ' ');
+        }
+
+        [TestMethod]
+        public void Test12()
+        {
+            var hightempLines = _hightempText.Split('\n');
+            File.WriteAllText("col1.txt", hightempLines[0]);
+            File.WriteAllText("col2.txt", hightempLines[1]);
+        }
+        
+    }
+}
--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 [第5回 N-gramのしくみ：検索エンジンを作る｜gihyo.jp … 技術評論社](http://gihyo.jp/dev/serial/01/make-findspot/0005?page=2)
 
 [全文検索 - Wikipedia](http://ja.wikipedia.org/wiki/%E5%85%A8%E6%96%87%E6%A4%9C%E7%B4%A2)
+
+[コマンドの使い方(Linux) | hydrocul のメモ](http://hydrocul.github.io/wiki/commands/)
