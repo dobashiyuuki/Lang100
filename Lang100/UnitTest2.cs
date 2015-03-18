31168b5b39db01eb91b1790de9d4a50e351bdfa6
nabehiro <v_hirow@sigmact.com>
2015-03-19 06:49:21 +0900

    ch2 end

--- Lang100/UnitTest2.cs
+++ Lang100/UnitTest2.cs
@@ -33,10 +33,79 @@ namespace Lang100
         [TestMethod]
         public void Test12()
         {
+            // cat hightemp.txt | cut -d$'\n' -f1,2
             var hightempLines = _hightempText.Split('\n');
             File.WriteAllText("col1.txt", hightempLines[0]);
             File.WriteAllText("col2.txt", hightempLines[1]);
         }
-        
+
+        [TestMethod]
+        public void Test13()
+        {
+            // paste コマンドがない・・・
+            var result = File.ReadAllText("col1.txt") + "\n" + File.ReadAllText("col2.txt");
+        }
+
+        [TestMethod]
+        public void Test14()
+        {
+            // head -n 10 hightemp.txt
+            var n = 10;
+            var hightempLines = _hightempText.Split('\n');
+            var result = string.Join("\n", hightempLines.Take(n));
+        }
+
+        [TestMethod]
+        public void Test15()
+        {
+            // tail -n 10 hightemp.txt
+            var n = 10;
+            var hightempLines = _hightempText.Split('\n');
+            var result = string.Join("\n",
+                hightempLines.Skip(Math.Max(0, hightempLines.Length - n)).Take(n));
+        }
+
+        [TestMethod]
+        public void Test16()
+        {
+            // split は、行数指定で分割。分割数指定ではない
+            var n = 5;
+
+            var hightempLines = _hightempText.Split('\n');
+            var rowCount = hightempLines.Length / n;
+            var result = new string[n];
+            for (var i = 0; i < n - 1; i++)
+                result[i] = string.Join("\n", hightempLines.Skip(i * rowCount).Take(rowCount));
+            result[n - 1] = string.Join("\n", hightempLines.Skip((n - 1) * rowCount));
+        }
+
+        [TestMethod]
+        public void Test17()
+        {
+            // cat hightemp.txt | cut -c 1-3 | sort | uniq
+            var hightempLines = _hightempText.Split('\n');
+            var result = hightempLines.Select(s => s[0]).Distinct().OrderBy(s => s);
+        }
+
+        [TestMethod]
+        public void Test18()
+        {
+            // cat hightemp.txt | sort -t $'\t' -k 3
+            var hightempLines = _hightempText.Split('\n');
+            var result = hightempLines.OrderBy(s => s.Split('\t')[2]);
+        }
+
+        [TestMethod]
+        public void Test19()
+        {
+            // shell わかんにゃい・・
+            var hightempLines = _hightempText.Split('\n');
+            var prefCountDic = hightempLines.Select(s => s.Split('\t')[0])
+                .GroupBy(p => p).ToDictionary(g => g.Key, g => g.Count());
+            var result = hightempLines
+                .OrderByDescending(s => prefCountDic[s.Split('\t')[0]])
+                .ThenBy(s => s.Split('\t')[0]);
+
+        }
     }
 }
